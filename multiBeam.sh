7402b7a0ebd2b732061476a535334961316f3aa5
Miquel Colom i Bernadich <mcbernadich@mpifr-bonn.mpg.de>
2021-09-26 16:54:34 +0200

    Corrected bug in multiBeam.sh

--- multiBeam.sh
+++ multiBeam.sh
@@ -128,7 +128,7 @@ for pointing in ${pointing_list[@]}; do
 	echo 'Going back to main directory.'
 	cd ..
 	#Loop over the two halves of an observation.
-	for half in $(seq -w 0 ${nfiles}); do
+	for half in $(seq -w 0 $( ls /data${path}/${pointing}/cfbf00000 | wc -l )); do
 		echo ""
 		echo ""
 		echo "Working on the ${half}-th part of pointing ${pointing}"
