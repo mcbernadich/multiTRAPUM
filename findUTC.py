18071f5a6cdcd8d625843d404ae697fe079e1d58
Miquel Colom i Bernadich <mcbernadich@mpifr-bonn.mpg.de>
2021-10-21 17:01:40 +0200

    Fixed big in findUTC.py

--- findUTC.py
+++ findUTC.py
@@ -5,6 +5,7 @@ import json
 #arg2 is what is to be read from the metafile.
 with open(sys.argv[1],"r") as f:
     meta=json.load(f)
-thing=str(meta[sys.argv[2]].split(",")[0])
+thing=str(meta[sys.argv[2]])
+thing=thing.split(",")[0])
 thing=thing.replace("/","-")
 print(thing)
